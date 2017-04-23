495edc6e2d2e62964542016fb35d4a0a42af97bc
Amit Papushe Shely <papushe9@gmail.com>
2017-04-23 17:20:13 +0300

    bug fix

--- app/footballScore/index.js
+++ app/footballScore/index.js
@@ -26,27 +26,27 @@ module.exports = class Football extends Emitter.EventEmitter{
     }
     plusGoal(goal){
         this.score += goal; // score++
-        this.dataLog.push(plusGoalim + this.score); // push the logs to dataLogs array
+        this.dataLog.push(`${plusGoalim} ${this.score}`); // push the logs to dataLogs array
         this.emit(eventsName.events.CHANGE); //emit (=fire) event
     }
     minusGoal(goal){
         if (this.score < 1){ // if the score less then 1 (0)
-            this.dataLog.push(cant + this.score); // push the logs to dataLogs array
+            this.dataLog.push(`${cant} ${this.score}`); // push the logs to dataLogs array
         }
         else {
             this.score -= goal; // score--
-            this.dataLog.push(minusGoalim + this.score); // push the logs to dataLogs array
+            this.dataLog.push(`${minusGoalim} ${this.score}`); // push the logs to dataLogs array
             this.emit(eventsName.events.CHANGE); //emit (=fire) event
         }
     }
     displayScores() {
-        console.log("The score is " + this.score) ; // callback function
+        console.log(`The score is ${this.score}`) ; // callback function
     }
     nameOfPlayer() {
-        console.log("The name is " + this.name) ; // callback function
+        console.log(`The name is ${this.name}`) ; // callback function
     }
     typeOfSports() {
-        console.log("The type of sports is " + this.sports) ; // callback function
+        console.log(`The type of sports is ${this.sports}`) ; // callback function
     }
     dataLogs(){
         return this.dataLog; // return the dataLogs array
